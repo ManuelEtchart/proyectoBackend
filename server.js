124eb25e677f0f458ea5715271779768a06f5339
Manuel Etchart <manuetchart@live.com.ar>
2022-02-08 19:34:57 -0300

    des websocket

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const fs = require('fs');
+const {Server: HttpServer} = require('http');
+const { Server: IOServer } = require('socket.io');
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
-const router = require('./routes.js')
+let productos = [];
+let mensajes = [];
+let contador = 1;
 
-app.use('/api/productos', router)
+io.on('connection', (socket) => {
+   console.log('Nuevo cliente conectado');
+
+   socket.emit('productos', {productos: productos});
+
+   socket.emit('mensajes', {mensajes: mensajes});
+
+   socket.on('productoIngresado', producto => {
+      producto.id = contador;
+      productos.push(producto);
+      contador = productos[productos.length - 1].id + 1;
+      io.sockets.emit('productos', {productos: productos});
+   });
+
+   socket.on('mensajeNuevo', mensaje => {
+      let fechaactual = new Date();
+      mensaje.fecha = `[(${fechaactual.getDay()}/${fechaactual.getMonth()}/${fechaactual.getFullYear()} ${fechaactual.getHours()}:${fechaactual.getMinutes()}:${fechaactual.getSeconds()})]`;
+      console.log(mensaje);
+      mensajes.push(mensaje);
+      fs.writeFileSync('./chat.txt', JSON.stringify(mensajes,null,2));
+      io.sockets.emit('mensajes', {mensajes: mensajes});
+   })
+})
 
 const PORT = 8080;
 
-const server = app.listen(PORT, () => {
+const server = httpServer.listen(PORT, () => {
    console.log(`Servidor escuchando en el puerto ${server.address().port}`);
 });
 
 server.on("error", error => console.log(`Error en servidor ${error}`));
-
